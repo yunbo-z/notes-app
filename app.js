6c25d74689204c267984978c4a3fa354ab15100c
yunbo-z <zyunbo0330@gmail.com>
2023-11-03 18:14:07 +0100

    test process.argv module

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-// require('./utils')
+const chalk = require('chalk')
+const validator = require('validator')
 
-// const name = 'Yunbo'
-
-// console.log(name)
 const getNotes = require('./notes.js')
 const msg = getNotes()
+
+const command = process.argv[2]
+if (command == 'add'){
+    console.log('Add Notes')
+}else if (command == 'remove'){
+    console.log('Remove Notes')
+}
+
 console.log(msg)
+console.log (validator.isEmail('yb@gmaol.com'))
+console.log(chalk.red.inverse.bold('error'))
